6daad3c108888ee0cef2ab71ff108c26a1236b5c
shubham-khandekar <shubhamkhandekar001@gmail.com>
2020-06-04 14:34:33 +0530

    adding windspeed property

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -12,7 +12,7 @@ const forecast=(latitude,longitude,callback)=>{
             callback("unable to find location",undefined)
         }else{
             // callback(undefined,`${response.body.current.weather_descriptions[0]} it is currently ${response.body.current.temperature} degrees out. it feels like ${response.body.current.feelslike} degrees out`)
-            callback(undefined,`${body.current.weather_descriptions[0]} it is currently ${body.current.temperature} degrees out. it feels like ${body.current.feelslike} degrees out`)
+            callback(undefined,`${body.current.weather_descriptions[0]} it is currently ${body.current.temperature} degrees out. it feels like ${body.current.feelslike} degrees out and it has wind speed ${body.current.wind_speed}`)
         }
 })
 }
